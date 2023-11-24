2a93f8cf2dbd56372725df7f7e60401837b7e8dd
sepolly <sepolly6@gmail.com>
2023-11-24 23:50:25 +0000

    first branch commit

--- Database/database.sql
+++ Database/database.sql
@@ -1,3 +1,4 @@
+-- Active: 1700672898166@@127.0.0.1@3306@todo
 
 
 CREATE TABLE IF NOT EXISTS users(
@@ -8,12 +9,12 @@ CREATE TABLE IF NOT EXISTS users(
     email VARCHAR(50),
     password VARCHAR(255),
     created_at DATETIME DEFAULT NOW(),
-    updated_at DATETIME
-
+    updated_at DATETIME DEFAULT NOW() ON UPDATE NOW()
 );
 
+
 CREATE TABLE IF NOT EXISTS todos(
-    todo_id INT PRIMARY KEY,
+    todo_id INT PRIMARY KEY AUTO_INCREMENT,
     title TEXT,
     description TEXT,
     start_date DATE,
@@ -21,8 +22,6 @@ CREATE TABLE IF NOT EXISTS todos(
     due_time TIME,
     user_id INT NOT NULL,
     created_at DATETIME DEFAULT NOW(),
-    updated_at DATETIME
-
+    updated_at DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
-ALTER TABLE todos ADD CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES users(user_id);
--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 
 function db(){
-    $con = new mysqli('localhost', 'root', 'A75294143b','todo',3306);
+    $con = new mysqli('localhost', 'root', '','todo',3306);
     if(!$con){
         die('not connected');
     }
